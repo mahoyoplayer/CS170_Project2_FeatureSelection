ed0e95a400f114ed9212311574cc99d7e8063156
kitty1737 <obesedoge@gmail.com>
2026-03-09 00:27:22 -0700

    First day's progress

--- first.py
+++ first.py
@@ -3,31 +3,55 @@ import math
 
 
 data = np.loadtxt("data120.txt")
+instanceCount, featureCount = data.shape
 
-
-labels, features = np.split(data, [1], axis = 1)
-featureCount = len(features)
-labels = labels.astype(int)
-print(labels)
-print(features)
-
-# Features is a set?
+# Features can be a set or list here.
 def accuracy(data, features, k = 5, bestAccuracy=0.0) -> float:
     def EuclidDist(inst1, inst2):
         total = 0
         for i in features:
             total += (inst1[i] - inst2[i]) ** 2
         return math.sqrt(total)
-    correctCount = 0
-    instanceCount = 9
-    bestLabel, minDistance = None, float("inf")
-    if correctCount / instanceCount < bestAccuracy:
-        # Alpha beta pruning
-        return -1.0
 
+    def getFolds():
+        # This does not work properly
+        foldSize = len(data) // k
+        folds = []
+        for i in range(k):
+            start = i * foldSize
+            end = start + foldSize
+            folds.append(data[start:end])
+        return folds
+
+    correctCount = 0
+    folds = getFolds()
+    for i, testFold in enumerate(folds):
+        trainFolds = []
+        for j in range(k):
+            if j == i: continue
+            trainFolds.extend(folds[j])
+
+        testLabels, testFeatures = testFold[:, 0], testFold[:, 1:]
+        trainLabels, trainFeatures = trainFolds[:, 0], trainFolds[:, 1:]
+        for i in range(len(testFeatures)):
+            testLabel = testLabels[i]
+            testFeature = testFeatures[i]
+            bestLabel, minDistance = None, float("inf")
+            for j in len(trainFeatures):
+                #trainLabel = trainLabels[j]
+                train = trainFeatures[i]
+                dist = EuclidDist(testFeature, train)
+                if dist < minDistance:
+                    minDistance = dist
+                    bestLabel = trainLabels[j]
+            if bestLabel == testLabel:
+                correctCount += 1
+            else:
+                # Pruning code goes here
+                pass
+                #return -1
     return correctCount / instanceCount
 
-#print(data.shape)
-#print(data[:2])
-
-#print("Hello World!")
+f = [8, 3, 5]
+test = accuracy(data, f)
+print(test)
